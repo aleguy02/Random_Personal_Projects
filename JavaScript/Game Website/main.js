d8305aa958b981c59ad57db3bf5eb01f010b64cd
Ale V <alejandro.the.villate@gmail.com>
2023-11-20 19:53:45 -0500

    reformated code from last commit

--- JavaScript/Game Website/main.js
+++ JavaScript/Game Website/main.js
@@ -8,33 +8,19 @@ const startButton = document.getElementById("start-button");
 const directionsContainer = document.getElementById("directions-container");
 const gameGrid = document.getElementById("game-grid");
 const gridElements = gameGrid.querySelectorAll(".grid-element");
-const winScreenContainer = document.getElementById("win-screen-container")
-console.log(gridElements)
+const winScreenContainer = document.getElementById("win-screen-container");
+console.log(gridElements);
 
 const initApp = () => {
   console.log("initApp");
   startButton.addEventListener("click", (event) => {
     directionsContainer.style.display = "none";
-    gameGrid.style.display = "grid"
+    gameGrid.style.display = "grid";
     defineGameElements();
 
-    // every time the game grid is clicked, build a new array of backgroundColors. Then, if 'red' is not in the array, switch screens
-    gameGrid.addEventListener("click", (event) => {
-      let backgroundColorArray = []
-      gridElements.forEach((item) => {
-        backgroundColorArray.push(item.style.backgroundColor)
-      })
-      if (backgroundColorArray.includes('red')) {
-        console.log('There is still a red box')
-      } else {
-        console.log('There are no red boxes')
-        gameGrid.style.display = "none"
-        winScreenContainer.style.display = "flex"
-      }
-    })
+    checkForWin();
   });
 };
-
 
 const defineGameElements = () => {
   console.log("Game started");
@@ -46,18 +32,32 @@ const defineGameElements = () => {
       ? (gridElement.style.backgroundColor = "red")
       : gridElement.style.backgroundColor;
 
-      gridElement.addEventListener("click", (event) => {
-        gridElement.style.backgroundColor === "red"
-          ? (gridElement.style.backgroundColor = "")
-          : alert("Nuh uh!");
-      })
+    gridElement.addEventListener("click", (event) => {
+      gridElement.style.backgroundColor === "red"
+        ? (gridElement.style.backgroundColor = "")
+        : alert("Nuh uh!");
+    });
   }
 };
 
+const checkForWin = () => {
+  // every time the game grid is clicked, build a new array of backgroundColors. Then, if 'red' is not in the array, switch screens
+  gameGrid.addEventListener("click", (event) => {
+    let backgroundColorArray = [];
+    gridElements.forEach((item) => {
+      backgroundColorArray.push(item.style.backgroundColor);
+    });
+    if (backgroundColorArray.includes("red")) {
+      console.log("There is still a red box");
+    } else {
+      console.log("There are no red boxes");
+      gameGrid.style.display = "none";
+      winScreenContainer.style.display = "flex";
+    }
+  });
+};
 
-// Functions that are included in the game but not the game logic itself
-
-const determineColor = () => {
+const determineColor = () => {  // included in the game but not the game logic itself
   let randomNum = Math.floor(Math.random() * 5) + 1;
   return randomNum;
 };
